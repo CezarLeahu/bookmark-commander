48caaaca3b96f77ccc6d6443dd726ab8279e8f6e
Cezar.Leahu <cezar.leahu@protonmail.com>
2022-11-19 20:55:05 +0200

    Expand bookmark api calls

--- src/bookmarks/queries.ts
+++ src/bookmarks/queries.ts
@@ -24,11 +24,14 @@ export const parentPath = async (node?: BTN): Promise<BTN[]> => {
   return parents
 }
 
-export const children = async (nodeID: string): Promise<BTN[]> => {
-  const node = (await bookmarks.get(nodeID))[0]
-  const childNodes = await bookmarks.getChildren(nodeID)
+export const children = async (id: string): Promise<BTN[]> => {
+  const node = (await bookmarks.get(id))[0]
+  if (node === undefined) {
+    throw new Error('Node does not exist')
+  }
+  const childNodes = await bookmarks.getChildren(id)
 
-  return node?.parentId === undefined
+  return node.parentId === undefined
     ? childNodes
     : [{ title: '..', id: node.parentId }, ...childNodes]
 }
@@ -39,4 +42,16 @@ export const getTopNodes = async (): Promise<BTN[]> => {
 
 export const getNode = async (id: string): Promise<BTN> => {
   return (await bookmarks.get(id))[0]
+}
+
+export const isNonEmptyDirectory = async (id: string): Promise<boolean> => {
+  return await containsNonEmptyDirectories([id])
+}
+
+export const containsNonEmptyDirectories = async (ids: string[]): Promise<boolean> => {
+  const nodes = await bookmarks.get(ids)
+  return (
+    nodes.find(e => e.url === undefined && e.children !== undefined && e.children.length > 0) !==
+    undefined
+  )
 }
